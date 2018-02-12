1a89eda7e52bf7ef643293ab2809195c344bea32
kalle hallden <35563440+KalleHallden@users.noreply.github.com>
2018-02-12 11:46:56 +0100

    first commit

---
+++ messagingApp/messagingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messagingApp
+//
+//  Created by kalle hallden on 2018-02-12.
+//  Copyright © 2018 Elite Athlete Coaching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
